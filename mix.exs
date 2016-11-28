f86a2ea6931e0d1e53a688c7bed5438f9df2c629
M. Peter <mp@tcs.de>
2016-11-28 15:32:21 +0100

    fixed elixir min. version

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule SortedTtlList.Mixfile do
 		[
 			app: :sorted_ttl_list,
 			version: "0.0.1",
-			elixir: "~> 1.3",
+			elixir: "~> 1.2",
 			build_embedded: Mix.env == :prod,
 			start_permanent: Mix.env == :prod,
 			deps: deps(),
